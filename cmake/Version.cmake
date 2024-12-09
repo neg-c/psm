62220b10ec70e9f87aa3c653cc071287aaa3eeeb
Genci Berisha <me@genciberisha.dev>
2024-12-09 20:54:56 +0100

    Bump version to v0.0.1-alpha

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -1,7 +1,14 @@
 include(CMakePackageConfigHelpers)
 
+if(DEFINED PROJECT_VERSION_SUFFIX)
+  set(FULL_PROJECT_VERSION "${PROJECT_VERSION}-${PROJECT_VERSION_SUFFIX}")
+else()
+  set(FULL_PROJECT_VERSION "${PROJECT_VERSION}")
+endif()
+
 write_basic_package_version_file(
   ${CMAKE_CURRENT_BINARY_DIR}/cmake/psmConfigVersion.cmake
+  VERSION ${PROJECT_VERSION}
   COMPATIBILITY AnyNewerVersion)
 
 configure_file("${CMAKE_CURRENT_SOURCE_DIR}/cmake/version.hpp.in"
