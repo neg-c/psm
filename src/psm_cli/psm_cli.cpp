94026bb3d1e140536ecf0df480ffeb3aabee23e1
Genci Berisha <me@genciberisha.dev>
2025-01-09 11:27:32 +0100

    Revert "psm_cli load image with opencv"
    This reverts commit 0761f2c9e0cf97ae9b757c6031faa94fb2fbd40a.

--- src/psm_cli/psm_cli.cpp
+++ src/psm_cli/psm_cli.cpp
@@ -1,8 +1,8 @@
 #include <cstddef>
 #include <format>
 #include <iostream>
-#include <opencv2/opencv.hpp>
 #include <ranges>
+#include <vector>
 
 #include "psm/psm.hpp"
 
@@ -14,53 +14,25 @@ void print_buffer(const Buffer& buffer) {
   std::cout << "\n";
 }
 
-static int window(const std::basic_string<char>& wndName, const cv::Mat& roi,
-                  bool freeze) {
-  cv::namedWindow(wndName, cv::WINDOW_NORMAL);
-  cv::setWindowProperty(wndName, cv::WND_PROP_FULLSCREEN, cv::WND_PROP_TOPMOST);
-  cv::imshow(wndName, roi);
-  if (freeze) {
-    while (true) {
-      int c = cv::waitKey();
-      if (c == 27) {
-        break;
-      }
-    }
-  }
-  return 0;
-}
+int main() {
+  std::vector<unsigned char> input_image = {
+      0,   0,   255,  // Blue pixel (B=0, G=0, R=255)
+      0,   255, 0,    // Green pixel (B=0, G=255, R=0)
+      255, 0,   0,    // Red pixel (B=255, G=0, R=0)
+      0,   255, 255   // Yellow pixel (B=0, G=255, R=255)
+  };
 
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " <image_path>\n";
-    return 1;
-  }
+  std::vector<unsigned char> output_image(input_image.size());
 
-  cv::Mat image = cv::imread(argv[1], cv::IMREAD_UNCHANGED);
-  cv::Mat out_image(image.rows, image.cols, CV_8UC3);
+  std::cout << "Input Image (BGR):\n";
+  print_buffer(input_image);
 
-  if (image.empty()) {
-    std::cerr << "Error: Could not load image " << argv[1] << "\n";
-    return 1;
-  }
+  psm::Convert<psm::sRGB, psm::oRGB>(input_image, output_image);
+  psm::AdjustChannels<psm::oRGB>(output_image, {0, 20, 20});
+  psm::Convert<psm::oRGB, psm::sRGB>(output_image, output_image);
 
-  // Use image directly without conversion
-  if (image.channels() != 3 && image.channels() != 4) {
-    std::cerr << "Error: Image must be RGB or RGBA\n";
-    return 1;
-  }
-
-  std::span<unsigned char> in_span(
-      image.ptr(), image.ptr() + image.total() * image.channels());
-  std::span<unsigned char> out_span(
-      out_image.ptr(),
-      out_image.ptr() + out_image.total() * out_image.channels());
-
-  psm::Convert<psm::sRGB, psm::oRGB>(in_span, out_span);
-  psm::AdjustChannels<psm::oRGB>(out_span, {0, 20, 20});
-  psm::Convert<psm::oRGB, psm::sRGB>(out_span, out_span);
-  window("kdfafjdalkf", image, false);
-  window("kfjdalkdfsadff", out_image, true);
+  std::cout << "Output Image (BGR):\n";
+  print_buffer(output_image);
 
   return 0;
 }
