808213f1df7c48915dbbf2004125635610043e4b
Genci Berisha <me@genciberisha.dev>
2025-01-29 14:34:51 +0100

    fix(test): copy gtest dll to the test executable if on windows
    Windows requires for the executable and its dependencies linked
    dynamically to be placed in the same directory.
    Later this needs to be fixed properly with RPATH

--- cmake/Testing.cmake
+++ cmake/Testing.cmake
@@ -8,6 +8,15 @@ include(GoogleTest)
 macro(AddTests target)
   target_link_libraries(${target} PRIVATE GTest::gtest GTest::gtest_main
                                           GTest::gmock)
+  if(MSVC)
+    add_custom_command(
+      TARGET ${target}
+      POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy -t $<TARGET_FILE_DIR:${target}>
+              $<TARGET_RUNTIME_DLLS:${target}>
+      COMMAND_EXPAND_LISTS)
+  endif()
+
   gtest_discover_tests(${target})
   # TODO: Add coverage
 endmacro()
