ad1091538a3129929117ab1c070fe6cd1fdef6a9
Genci Berisha <me@genciberisha.dev>
2025-05-14 15:10:29 +0200

    feat(toolbar): Layout for ComboBox and Reset,Save buttons

--- src/psm_gui/ui/panels/Toolbar.cpp
+++ src/psm_gui/ui/panels/Toolbar.cpp
@@ -1,5 +1,7 @@
 #include "Toolbar.hpp"
 
+#include <imgui.h>
+
 namespace psm_gui::ui::panels {
 
 void Toolbar::draw(AppState& s, const PanelRect& r) {
@@ -8,7 +10,49 @@ void Toolbar::draw(AppState& s, const PanelRect& r) {
   ImGui::SetNextWindowPos(r.pos);
   ImGui::SetNextWindowSize(r.size);
   ImGui::Begin("Toolbar", nullptr, flags);
-  // … buttons, menus, etc …
+
+  ImGuiStyle& style = ImGui::GetStyle();
+  float btnW = 80.0f;
+  float spacing = style.ItemSpacing.x;
+  float totalW = (btnW * 2) + spacing;
+  float comboW = r.size.x * 0.3f;
+
+  float contentHeight = r.size.y - (style.WindowPadding.y * 2);
+  float frameHeight = ImGui::GetFrameHeight();
+  float offsetY =
+      style.WindowPadding.y + ((contentHeight - frameHeight) * 0.5f);
+  ImGui::SetCursorPosY(offsetY);
+
+  if (ImGui::BeginTable("##ToolbarTable", 3, ImGuiTableFlags_NoPadInnerX)) {
+    ImGui::TableSetupColumn("##Combo", ImGuiTableColumnFlags_WidthFixed,
+                            comboW);
+    ImGui::TableSetupColumn("##Filler", ImGuiTableColumnFlags_WidthStretch);
+    ImGui::TableSetupColumn("##Buttons", ImGuiTableColumnFlags_WidthFixed,
+                            totalW);
+    ImGui::TableNextRow();
+    ImGui::TableNextColumn();
+    ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 20.0f);
+    ImGui::PushItemWidth(-FLT_MIN);
+    static const char* names[] = {"sRGB", "AdobeRGB", "DisplayP3"};
+    static int current_item = 0;
+    ImGui::Combo("Color Space", &current_item, names, IM_ARRAYSIZE(names));
+    ImGui::PopItemWidth();
+
+    ImGui::TableNextColumn();
+
+    ImGui::TableNextColumn();
+    float rightPadding = -20.0f;
+    float buttonsWidth = totalW - rightPadding;
+    ImGui::SetCursorPosX(ImGui::GetCursorPosX() + (totalW - buttonsWidth));
+    if (ImGui::Button("Reset", ImVec2(btnW, 0))) { /*…*/
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("Save", ImVec2(btnW, 0))) { /*…*/
+    }
+
+    ImGui::EndTable();
+  }
+
   ImGui::End();
 }
 
