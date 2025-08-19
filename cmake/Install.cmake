66c0187c24c368273d20c6ddfef1e0679377d581
Genci Berisha <me@genciberisha.dev>
2025-08-19 23:16:59 +0200

    style(cmake): cmake-format run

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -29,9 +29,10 @@ if(BUILD_PSM_GUI)
   install(TARGETS psm_gui RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
                                   COMPONENT applications)
 
-  install(FILES $<TARGET_RUNTIME_DLLS:psm_gui>
-          DESTINATION ${CMAKE_INSTALL_BINDIR}
-          COMPONENT applications)
+  install(
+    FILES $<TARGET_RUNTIME_DLLS:psm_gui>
+    DESTINATION ${CMAKE_INSTALL_BINDIR}
+    COMPONENT applications)
 endif()
 
 # Run ldconfig on Unix systems
